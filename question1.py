f3ec01c51aa7c7d3a7063a93077d3e3cc691cdd4
CynicalBanana123 <s384873@students.cdu.edu.au>
2025-08-30 13:23:41 +0930

    The new and complete version

--- question1.py
+++ question1.py
@@ -1,150 +1,128 @@
 import os
 
-#User input for shift values
-shiftInput1 = input("Enter shift1 value: ")
-shiftInput2 = input("Enter shift2 value: ")
-
 def encrypt(shift1, shift2):
-    #Alphabet
-    lowercase = 'abcdefghijklmnopqrstuvwxyz'
-    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' 
+
+    lowercase1 = "abcdefghijklm"
+    lowercase2 = "nopqrstuvwxyz"
+    uppercase1 = "ABCDEFGHIJKLM"
+    uppercase2 = "NOPQRSTUVWXYZ"
     message = ""
 
-    #Converting to integer
-    shift1Int = int(shift1)
-    shift2Int = int(shift2)
+    #Converting string shift1 and shift2 to int
+    shift1 = int(shift1)
+    shift2 = int(shift2)
 
-    with open('raw_text.txt') as f: 
+    with open("raw_text.txt") as f:
         for line in f:
             for char in line:
-                pos1 = lowercase.find(char)
-                pos2 = uppercase.find(char)
-            
-                #If character is found in lowercase alphabet.. we use -1 because 0=a, meaning that 0 is the start of the alphabet
-                if pos1 > -1:
+                #Getting position (index) of character in alphabet
+                position1 = lowercase1.find(char)
+                position2 = lowercase2.find(char)
+                position3 = uppercase1.find(char)
+                position4 = uppercase2.find(char)
+
+                #If character found in lowercase1 string
+                if position1 > -1:
+                    position1 = ((position1 + (shift1*shift2))%len(lowercase1))
+                    message+=lowercase1[position1]
+
+                #If character found in lowercase2 string
+                elif position2 > -1:
+                    position2 = ((position2 - (shift1+shift2))%len(lowercase2))
+                    message+=lowercase2[position2]
                 
-                    #Character is in a - m
-                    if 'a' <= char <= 'm':                
-                        newPos1 = pos1 + (shift1Int*shift2Int)
-                        #If new value is greater than the length of alphabet, we need to add its remainder from the start to prevent string index out of range
-                        if newPos1 >= len(lowercase):
-                            remaining = newPos1 % len(lowercase)
-                            message += lowercase[remaining]
-                        elif newPos1 <= len(lowercase):
-                            message += lowercase[newPos1]
-                        
-                    #Character is in n - z      
-                    elif 'n' <= char <= 'z':
-                        newPos1 = pos1 - (shift1Int*shift2Int)
-                        if newPos1 >= len(lowercase):
-                            remaining = newPos1 % len(lowercase)
-                            message += lowercase[remaining]
-                        elif newPos1 <= len(lowercase):
-                            message += lowercase[newPos1]
-                        
-                elif pos2 > -1:
-                    
-                    #Character is in A - M
-                    if 'A' <= char <= 'M':
-                        newPos2 = pos2 - shift1Int
-                        #Using remainder to prevent string index out of range
-                        if newPos2 < 0:
-                            remaining = newPos2 % len(uppercase)
-                            message += uppercase[remaining]
-                        else:
-                            message += uppercase[newPos2]
-                    
-                    #Character is in N - Z
-                    if 'N' <= char <= 'Z':
-                        newPos2 = pos2 + (shift2Int*shift2Int)
-                        #Using remainder to prevent string index out of range
-                        if newPos2 >= len(uppercase):
-                            remaining = newPos2 % len(uppercase)
-                            message += uppercase[remaining]
-                        elif newPos2 <= len(uppercase):
-                            message += uppercase[newPos2]
+                #If character found in uppercase1 string
+                elif position3 > -1:
+                    position3 = ((position3 - shift1)%len(uppercase1))
+                    message+=uppercase1[position3]
                 
-                #To keep the spaces/other characters like grammar, this else statement adds characters that don't need to be encrypted.
+                #If character found in uppercase2 string
+                elif position4 > -1:
+                    position4 = ((position4 + (shift2*shift2)%len(uppercase2)))
+                    message+=uppercase2[position4]
+
+                #If no character found in string, then add what is originally in the raw_text.txt
                 else:
-                    message+=char
-                    
-    #Adding encrypted text to the file 'encrypted_text.txt'
-    e = open("encrypted_text.txt", 'a')   
-    e.write(message)
-    e.close() 
+                    message += char
     
-#Decrypt is the exact same code, except it is modified so that the positions are reversed, so we can decrypt.
+    #Writing encrypted message to 'encrypted_text.txt'
+    with open("encrypted_text.txt", 'a') as x:
+        x.write(message)
+        print("Message encrypted to 'encrypted_text.txt'.")
+        x.close()
+
 def decrypt(shift1, shift2):
-    #Alphabet
-    lowercase = 'abcdefghijklmnopqrstuvwxyz'
-    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' 
+    lowercase1 = "abcdefghijklm"
+    lowercase2 = "nopqrstuvwxyz"
+    uppercase1 = "ABCDEFGHIJKLM"
+    uppercase2 = "NOPQRSTUVWXYZ"
     message = ""
 
-    #Converting to integer
-    shift1Int = int(shift1)
-    shift2Int = int(shift2)
+    #Converting string shift1 and shift2 to int
+    shift1 = int(shift1)
+    shift2 = int(shift2)
 
-    #Reads from 'encrypted_text.txt'
-    with open('encrypted_text.txt') as f: 
+    with open("encrypted_text.txt") as f:
         for line in f:
             for char in line:
-                pos1 = lowercase.find(char)
-                pos2 = uppercase.find(char)
-            
-            #If character is found in lowercase alphabet.. we use -1 because 0=a, meaning that 0 is the start of the alphabet
-                if pos1 > -1:
+                #Getting position (index) of character in alphabet
+                position1 = lowercase1.find(char)
+                position2 = lowercase2.find(char)
+                position3 = uppercase1.find(char)
+                position4 = uppercase2.find(char)
+
+                #If character found in lowercase1 string
+                if position1 > -1:
+                    position1 = ((position1 - (shift1*shift2))%len(lowercase1))
+                    message+=lowercase1[position1]
+
+                #If character found in lowercase2 string
+                elif position2 > -1:
+                    position2 = ((position2 + (shift1+shift2))%len(lowercase2))
+                    message+=lowercase2[position2]
                 
-                    #Character is in a - m
-                    if 'a' <= char <= 'm':                
-                        newPos1 = pos1 - (shift1Int*shift2Int)
-                        #If new value is greater than the length of alphabet, we need to add its remainder from the start to prevent string index out of range
-                        if newPos1 >= len(lowercase):
-                            remaining = newPos1 % len(lowercase)
-                            message += lowercase[remaining]
-                        elif newPos1 <= len(lowercase):
-                            message += lowercase[newPos1]
-                        
-                    #Character is in n - z      
-                    elif 'n' <= char <= 'z':
-                        newPos1 = pos1 + (shift1Int*shift2Int)
-                        if newPos1 >= len(lowercase):
-                            remaining = newPos1 % len(lowercase)
-                            message += lowercase[remaining]
-                        elif newPos1 <= len(lowercase):
-                            message += lowercase[newPos1]
-                        
-                elif pos2 > -1:
-                    
-                    #Character is in A - M
-                    if 'A' <= char <= 'M':
-                        newPos2 = pos2 + shift1Int
-                        #Using remainder to prevent string index out of range
-                        if newPos2 < 0:
-                            remaining = newPos2 % len(uppercase)
-                            message += uppercase[remaining]
-                        else:
-                            message += uppercase[newPos2]
-                    
-                    #Character is in N - Z
-                    if 'N' <= char <= 'Z':
-                        newPos2 = pos2 - (shift2Int*shift2Int)
-                        #Using remainder to prevent string index out of range
-                        if newPos2 >= len(uppercase):
-                            remaining = newPos2 % len(uppercase)
-                            message += uppercase[remaining]
-                        elif newPos2 <= len(uppercase):
-                            message += uppercase[newPos2]
+                #If character found in uppercase1 string
+                elif position3 > -1:
+                    position3 = ((position3 + shift1)%len(uppercase1))
+                    message+=uppercase1[position3]
                 
-                #To keep the spaces/other characters like grammar, this else statement adds characters that don't need to be encrypted.
+                #If character found in uppercase2 string
+                elif position4 > -1:
+                    position4 = ((position4 - (shift2*shift2)%len(uppercase2)))
+                    message+=uppercase2[position4]
+
+                #If no character found in string, then add what is originally in the encrypted_text.txt
                 else:
-                    message+=char
-                
-                    
-    #Adding encrypted text to the file 'decrypted_text.txt'
-    e = open("decrypted_text.txt", 'a')   
-    e.write(message)
-    e.close() 
+                    message += char
     
-encrypt(shiftInput1, shiftInput2)
-decrypt(shiftInput1, shiftInput2)
-                            
+    #Writing encrypted message to 'decrypted_text.txt'
+    with open("decrypted_text.txt", 'a') as x:
+        x.write(message)
+        x.close()
+        print("Message encrypted to 'encrypted_text.txt'.")
+
+def verify():
+    with(open("raw_text.txt") as txt1, open("decrypted_text.txt") as txt2):
+
+        #While loop to see if characters from both files match
+        while True:
+            raw = txt1.read(1)
+            decrypted = txt2.read(1)
+
+            #If raw text doesn't match decrypted text, then print that it fails and end loop
+            if raw != decrypted:
+                print("Raw text does not match decrypted text. Verification failed!")
+                return
+            #Using not to detect end of string, meaning that both files match successfully.
+            elif not raw and not decrypted:
+                print("Raw text matches decrypted text. Verification successful!")
+                return
+            
+#=========Main (running program from here)===========
+shift1 = input("Enter value 1: ")
+shift2 = input("Enter value 2: ")
+encrypt(shift1, shift2)
+decrypt(shift1, shift2)
+verify()
+
+    
